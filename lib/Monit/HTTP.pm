33e70e37611772aacb518192cc0fca790771881e
Dean Hamstead <dean@fragfest.com.au>
2016-02-05 18:20:39 +1100

    Freshen things up, use HTTP::Tiny

--- lib/Monit/HTTP.pm
+++ lib/Monit/HTTP.pm
@@ -1,4 +1,15 @@
+#!/bin/false
+
+use warnings;
+use strict;
+
 package Monit::HTTP;
+
+our $VERSION = '0.01';
+
+=pod
+
+=encoding utf-8
 
 =head1 NAME
 
@@ -6,12 +17,9 @@ Monit::HTTP - an OOP interface to Monit.
 
 =cut
 
-use LWP::UserAgent;
+use HTTP::Tiny;
 use XML::Bare;
 use Error qw(:try);
-
-use warnings;
-use strict;
 
 use constant {
     TYPE_FILESYSTEM => 0,
@@ -34,8 +42,8 @@ use Exporter;
 
 our @EXPORT_OK = (
     'TYPE_FILESYSTEM',
-    'TYPE_DIRECTORY', 
-    'TYPE_FILE', 
+    'TYPE_DIRECTORY',
+    'TYPE_FILE',
     'TYPE_PROCESS',
     'TYPE_HOST',
     'TYPE_SYSTEM',
@@ -50,8 +58,8 @@ our @EXPORT_OK = (
 
 our %EXPORT_TAGS = ( constants => [
     'TYPE_FILESYSTEM',
-    'TYPE_DIRECTORY', 
-    'TYPE_FILE', 
+    'TYPE_DIRECTORY',
+    'TYPE_FILE',
     'TYPE_PROCESS',
     'TYPE_HOST',
     'TYPE_SYSTEM',
@@ -67,20 +75,11 @@ our %EXPORT_TAGS = ( constants => [
 our @ISA = qw(Exporter);
 our @EXPORT = qw(get_services command_run);
 
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
 =head1 SYNOPSIS
 
 This module exposes an interface to talk with Monit via its HTTP interface.
 You can use it to get the status of all the monitored services on that particular
-host such as CPU and Memory usage, current PID, parent PID, current running status, 
+host such as CPU and Memory usage, current PID, parent PID, current running status,
 current monitoring status and so on.
 The module can be used also for performing actions like:
 
@@ -92,11 +91,11 @@ The module can be used also for performing actions like:
 
     use Monit::HTTP ':constants';
 
-    my $hd = new Monit::HTTP(
+    my $hd = Monit::HTTP->new(
             hostname => '127.0.0.1',
             port     => '2812',
             use_auth => 1,
-            username => 'admin', 
+            username => 'admin',
             password => 'monit',
             );
 
@@ -104,8 +103,9 @@ The module can be used also for performing actions like:
         my @processes = $hd->get_services(TYPE_PROCESS);
         $hd->command_run($processes[0], ACTION_STOP);
         my $service_status_href = $hd->service_status($processes[0]);
-    } or do {
-            print $@;
+    }
+    or do {
+        print $@;
     };
 
 
@@ -133,13 +133,13 @@ They are meant to be used as arguments of the methods.
 
 =head1 METHODS
 
-=head2 C<$monit = new Monit::HTTP (...)>
+=head2 C<$monit = Monit::HTTP-E<gt>new(...)>
 
 Constructor.
 Create a new C<Monit::HTTP> object.
 This constructor can be called passing a list of various parameters:
 
-    my $monit = new Monit::HTTP (
+    my $monit = Monit::HTTP->new(
                     hostname => localhost,
                     port => 2812,
                     use_auth => 1,
@@ -148,7 +148,7 @@ This constructor can be called passing a list of various parameters:
 
 The values showed above are the default values in case no argument
 is passed to the constructor.
-If use_auth is equal to 1 (true) and username and password are not null the http 
+If use_auth is equal to 1 (true) and username and password are not null the http
 request will be peformed using those usernames and password (basic http auth).
 Be aware that if you provide username and password and you don't set
 use_auth to be 1 authentication won't work.
@@ -164,20 +164,34 @@ sub new {
     bless $self, $class;
 
     # set some defaults, if not already set
-    $self->{hostname} ||= "localhost";
+    $self->{hostname} ||= 'localhost';
     $self->{port} ||= 2812;
-    $self->{status_url} = 
-        "http://".$self->{hostname}.":".$self->{port}."/_status?format=xml";
     $self->{use_auth} ||= 0;
     if($self->{use_auth}) {
-        $self->{username} ||= "admin";
-        $self->{password} ||= "monit";
+        $self->{username} ||= 'admin';
+        $self->{password} ||= 'monit';
     }
 
-    return $self;
+    $self->{ua} = HTTP::Tiny->new( agent => sprintf('Perl %s/%s',__PACKAGE__,$VERSION) );
+    $self->_generate_url;
+
+    return $self
 }
 
-=head2 C<Monit::HTTP-E<gt>set_hostname($hostname)>
+sub _generate_url {
+
+    my $self = shift;
+
+    my $auth = '';
+    if (defined $self->{username} and defined $self->{password} and $self->{use_auth}) {
+        $auth = sprintf('%s:%s@',$self->{username},$self->{password})
+    }
+
+    $self->{status_url} = sprintf('http://%s%s:%d/_status?format=xml',
+                               $auth, $self->{hostname}, $self->{port});
+}
+
+=head2 C<$monit-E<gt>set_hostname($hostname)>
 
 Set the hostname of the monit instance
 
@@ -186,11 +200,11 @@ Set the hostname of the monit instance
 sub set_hostname {
     my ($self, $hostname) = @_;
     $self->{hostname} = $hostname;
-    $self->{status_url} = 
-        "http://".$self->{hostname}.":".$self->{port}."/_status?format=xml";
+    $self->_generate_url;
+    return $hostname
 }
 
-=head2 C<Monit::HTTP-E<gt>set_port($port)>
+=head2 C<$monit-E<gt>set_port($port)>
 
 Set the tcp port of the monit instance
 
@@ -199,11 +213,11 @@ Set the tcp port of the monit instance
 sub set_port {
     my ($self, $port) = @_;
     $self->{port} = $port;
-    $self->{status_url} = 
-        "http://".$self->{hostname}.":".$self->{port}."/_status?format=xml";
+    $self->_generate_url;
+    return $port
 }
 
-=head2 C<Monit::HTTP-E<gt>set_username($username)>
+=head2 C<$monit-E<gt>set_username($username)>
 
 Set the username to be used in thee basic http authentication
 
@@ -212,9 +226,11 @@ Set the username to be used in thee basic http authentication
 sub set_username {
     my ($self, $username) = @_;
     $self->{username} = $username;
+    $self->_generate_url;
+    return $username
 }
 
-=head2 C<Monit::HTTP-E<gt>set_password($password)>
+=head2 C<$monit-E<gt>set_password($password)>
 
 Set the password to be used in thee basic http authentication
 
@@ -223,56 +239,51 @@ Set the password to be used in thee basic http authentication
 sub set_password {
     my ($self, $password ) = @_;
     $self->{password} = $password;
+    $self->_generate_url;
+    return $password
 }
 
-=head2 C<$res = Monit::HTTP-E<gt>_fetch_info()>
+=head2 C<$res = $monit-E<gt>_fetch_info()>
 
-Called bye C<Monit::HTTP->get_services()>.
+Called by L</get_services>.
 Does not need to be called by user. This is a private (internal) method
 This private function connects via http (GET) to the monit server.
 
 URL requested is http://<hostname>:<port>/_status?format=xml
 
-An XML file is returned and parsed using XML::Bare.
+An XML file is returned and parsed using L<XML::Bare>.
 
-The raw XML data is stored in the object using the _set_xml() method.
-The raw XML data can be retrieved using _get_xml.
+The raw XML data is stored in the object using the L</_set_xml> method.
+The raw XML data can be retrieved using L</_get_xml>.
 
-An hash reference of the XML data (as the one returned by the parse_xml function of
-XML::Bare) is stored in the object.
+An hash reference of the XML data (as the one returned by the L<parse_xml|XML::Bare/parse_xml> function of
+L<XML::Bare>) is stored in the object.
 
 =cut
 
 sub _fetch_info {
     my ($self) = @_;
 
-    $self->{ua} = LWP::UserAgent->new;
-    $self->{ua}->agent("Perl Monit::HTTP/$VERSION");
-
-    my $req = HTTP::Request->new(GET => $self->{status_url});
-    if (defined $self->{username} and defined $self->{password} and $self->{use_auth}) {
-        $req->authorization_basic($self->{username},$self->{password});
-    }
-
     try {
-        my $res = $self->{ua}->request($req);
-        if ($res->is_success) {
-            $self->_set_xml($res->content);
-            my $xml = new XML::Bare(text => $self->_get_xml);
+        my $res = $self->{ua}->get( $self->{status_url} );
+        if ($res->{success}) {
+            $self->_set_xml($res->{content});
+            my $xml = XML::Bare->new(text => $self->_get_xml);
             $self->{xml_hash} = $xml->parse();
-        } else {
-            my $err = "Error while connecting to ".
-                $self->{status_url}." !\nError details: $@";
+        }
+        else {
+            my $err = 'Error while connecting to '.
+                $self->{status_url}." !\nError details: $res->{status} $res->{reason}";
             throw Error::Simple($err);
         }
-    } 
+    }
     catch Error with {
         my $ex = shift;
         $ex->throw();
     };
 }
 
-=head2 C<$res = Monit::HTTP-E<gt>get_services()>
+=head2 C<$res = $monit-E<gt>get_services()>
 
 Return an array of services configured on the remote monit daemon.
 
@@ -283,7 +294,7 @@ In case of any exepction an error is thrown and undef is returned.
 sub get_services {
     my ($self, $type) = @_;
     my @services;
-    $type ||= "-1";
+    $type ||= '-1';
 
     if ($type != TYPE_FILESYSTEM and
         $type != TYPE_DIRECTORY and
@@ -292,10 +303,10 @@ sub get_services {
         $type != TYPE_HOST and
         $type != TYPE_SYSTEM and
         $type != TYPE_FIFO and
-        $type != TYPE_STATUS and 
+        $type != TYPE_STATUS and
         $type != -1 ) {
 
-            throw Error::Simple("Don't understand this service type!");
+            throw Error::Simple('Don\'t understand this service type!');
             return undef;
     }
 
@@ -309,10 +320,10 @@ sub get_services {
     return @services;
 }
 
-=head2 C<$res = Monit::HTTP-E<gt>_set_xml($xml)>
+=head2 C<$res = $monit-E<gt>_set_xml($xml)>
 
 Private method to set raw xml data.
-Called from C<Monit::HTTP->_fetch_info()>
+Called from L</_fetch_info>
 
 =cut
 
@@ -321,10 +332,10 @@ sub _set_xml {
     $self->{status_raw_content} = $xml;
 }
 
-=head2 C<$res = Monit::HTTP-E<gt>_get_xml($xml)>
+=head2 C<$res = $monit-E<gt>_get_xml($xml)>
 
 Private method to get raw xml data.
-Called from C<Monit::HTTP->_fetch_info()>
+Called from L</_fetch_info>
 
 =cut
 
@@ -333,13 +344,12 @@ sub _get_xml {
     return $self->{status_raw_content};
 }
 
-=head2 C<$hashref_tree =
-Monit::HTTP-E<gt>service_status($servicename)>
+=head2 C<$hashref_tree = $monit-E<gt>service_status($servicename)>
 
-Returns the status for a particular service in form of hash with all the info 
+Returns the status for a particular service in form of hash with all the info
 for that service.
 Return undef is the service does not exists.
-To know the structure of the hash ref use Data::Dumper :D
+To know the structure of the hash ref use L<Data::Dumper> :D
 
 =cut
 
@@ -376,11 +386,12 @@ sub service_status {
 
     if (! scalar keys %$status_href) {
         throw Error::Simple("Service $service does not exist");
-        return undef;
-    } else { return $status_href; }
+        return
+    }
+    else { return $status_href }
 }
 
-=head2 C<Monit::HTTP-E<gt>command_run($servicename, $command)>
+=head2 C<$monit-E<gt>command_run($servicename, $command)>
 
 Perform an action against a service.
 $command can be a constant (ACTION_STOP, ACTION_START, ACTION_RESTART, ACTION_MONITOR, ACTION_UNMONITOR)
@@ -398,34 +409,26 @@ sub command_run {
         $command ne ACTION_MONITOR and
         $command ne ACTION_UNMONITOR ) {
 
-            throw Error::Simple("Don't understand this action");
-            return;
+            throw Error::Simple('Don\'t understand this action');
+            return
     }
 
     if(not defined $service) {
         $self->{is_success} = 0;
-        throw Error::Simple "Service not specified";
-        return;
+        throw Error::Simple 'Service not specified';
+        return
     }
 
     # if services does not exist throw error
 
-    my $url = "http://" . $self->{hostname} . ":" . $self->{port} . "/" . $service;
+    my $url = 'http://'.$self->{hostname}.':'.$self->{port}.'/'.$service;
 
-    my $req = HTTP::Request->new(POST => $url);
-    push @{ $self->{ua}->requests_redirectable }, 'POST';
-    $req->content_type('application/x-www-form-urlencoded');
-    $req->content("action=$command");
-
-    if (defined $self->{username} and defined $self->{password}) {
-        $req->authorization_basic($self->{username},$self->{password});
-    }
     try {
-        my $res = $self->{ua}->request($req);
-        if (! $res->is_success) {
-            throw Error::Simple($res->status_line);
+        my $res = $self->{ua}->post_form($url, { action => $command });
+        if (! $res->{success}) {
+            throw Error::Simple($res->{status});
         }
-    } 
+    }
     catch Error with {
         my $ex = shift;
         $ex->throw();
@@ -471,6 +474,9 @@ L<http://search.cpan.org/dist/Monit-HTTP-API>
 
 =back
 
+=head1 VERSION
+
+Version 0.02
 
 =head1 ACKNOWLEDGEMENTS
 
